dc817755feb668b76cb7ee04d8707a54a8420165
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-02-08 14:57:35 -0200

    Update GeneratorCrud.php

--- GeneratorCrud.php
+++ GeneratorCrud.php
@@ -14,6 +14,7 @@ use App\Core\Model;
  * @author Luciano Charles de Souza
  * E-mail: souzacomprog@gmail.com
  * Github: https://github.com/LucianoCharlesdeSouza
+ *YouTube: https://www.youtube.com/channel/UC2bpyhuQp3hWLb8rwb269ew?view_as=subscriber
  * @version 1.0.0
  * @since 2017-10-24
  */
